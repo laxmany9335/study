e768ab90bb40882bcf8d44cde43c4aca97c7c5cd
Laxman Yadav <laxmany9335@.com>
2025-04-19 09:30:17 +0530

    Cart page code done

--- server/index.js
+++ server/index.js
@@ -26,9 +26,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000", 
-      "https://localhost:3000",
-     " https://study-ashy-chi.vercel.app/"
+     "https://studynotion-beryl-omega.vercel.app/"
     ],
     credentials: true,
   })
--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://study-notion-bkjp.onrender.com/api/v1";
+const BASE_URL = "https://study-11.onrender.com/api/v1";
 
 //auth api
 export const endpoints = {
